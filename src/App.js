5686f74b49f65194ec9d3daa4fd2175f249e6ac5
RakeshKumarA <rkakumar85@gmail.com>
2021-04-26 09:04:10 +0530

    Added Media

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Swiper from 'react-id-swiper';
-import 'swiper/swiper-bundle.css';
-import Grid from '@material-ui/core/Grid';
-import { Card, CardContent } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Swiper from "react-id-swiper";
+import "swiper/swiper-bundle.css";
+import Grid from "@material-ui/core/Grid";
+import image1 from "./images/pexels-pixabay-414171.jpg";
+import image2 from "./images/pexels-martin-damboldt-814499.jpg";
+import image3 from "./images/pexels-pixabay-147411.jpg";
+import { Card, CardContent, CardMedia } from "@material-ui/core";
 import SwiperCore, {
   Navigation,
   Pagination,
@@ -11,7 +14,7 @@ import SwiperCore, {
   EffectCube,
   EffectCoverflow,
   Autoplay,
-} from 'swiper';
+} from "swiper";
 SwiperCore.use([
   Navigation,
   Pagination,
@@ -23,85 +26,52 @@ SwiperCore.use([
 
 const useStyles = makeStyles({
   parent: {
-    display: 'grid',
-    gridTemplateRows: 'auto 1fr auto',
-    height: '100vh',
+    display: "grid",
+    gridTemplateRows: "auto 1fr auto",
+    height: "100vh",
+  },
+  media: {
+    height: "600px",
+  },
+  mediaStyle: {
+    width: "100%",
+    height: "600px",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
   },
 });
 
 const App = () => {
   const classes = useStyles();
   const params = {
-    spaceBetween: 30,
-    slidesPerView: 3,
+    // spaceBetween: 30,
+    slidesPerView: 1,
     autoplay: {
-      delay: 1000,
+      delay: 3000,
       disableOnInteraction: false,
     },
     pagination: {
-      el: '.swiper-pagination',
+      el: ".swiper-pagination",
       clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
     },
   };
 
   return (
-    <main>
-      <Grid container justify="center">
-        <Grid item sm={8}>
-          <Swiper {...params}>
-            <div>
-              <Card style={{ backgroundColor: 'green' }}>
-                <CardContent>
-                  <h1>This is first</h1>
-                  <h2>This is Second</h2>
-                  <h3>This is Third</h3>
-                </CardContent>
-              </Card>
-            </div>
-            <div>
-              <Card style={{ backgroundColor: 'yellow' }}>
-                <CardContent>
-                  <h1>This is first</h1>
-                  <h2>This is Second</h2>
-                  <h3>This is Third</h3>
-                </CardContent>
-              </Card>
-            </div>
-            <div>
-              <Card style={{ backgroundColor: 'red' }}>
-                <CardContent>
-                  <h1>This is first</h1>
-                  <h2>This is Second</h2>
-                  <h3>This is Third</h3>
-                </CardContent>
-              </Card>
-            </div>
-            <div>
-              <Card style={{ backgroundColor: 'blue' }}>
-                <CardContent>
-                  <h1>This is first</h1>
-                  <h2>This is Second</h2>
-                  <h3>This is Third</h3>
-                </CardContent>
-              </Card>
-            </div>
-            <div>
-              <Card style={{ backgroundColor: 'gray' }}>
-                <CardContent>
-                  <h1>This is first</h1>
-                  <h2>This is Second</h2>
-                  <h3>This is Third</h3>
-                </CardContent>
-              </Card>
-            </div>
-          </Swiper>
-        </Grid>
-      </Grid>
-    </main>
+    <Swiper {...params}>
+      <div
+        className={classes.mediaStyle}
+        style={{ backgroundImage: `url(${image1})` }}
+      ></div>
+      <div
+        className={classes.mediaStyle}
+        style={{ backgroundImage: `url(${image2})` }}
+      ></div>
+      <div
+        className={classes.mediaStyle}
+        style={{ backgroundImage: `url(${image3})` }}
+      ></div>
+    </Swiper>
   );
 };
 
